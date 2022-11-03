fd85d2af52a52e20464239765949c5c52dbf4187
Giorgio <gio.ivaldi@gmail.com>
2022-11-03 20:02:22 +0100

    log frag begin

--- MainApp/app/src/main/java/com/example/myapp/activities/SplashActivity.kt
+++ MainApp/app/src/main/java/com/example/myapp/activities/SplashActivity.kt
@@ -1,12 +1,22 @@
 package com.example.myapp.activities
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.myapp.R
+import com.example.myapp.models.FirebaseWrapper
 
 class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val firebaseWrapper : FirebaseWrapper = FirebaseWrapper()
+        if (!firebaseWrapper.isAuthenticated()){
+            val intent = Intent(this,SignInActivity::class.java)
+            this.startActivity(intent)
+            finish()
+        }
+        //TODO: Check if is an instructor
     }
 }
