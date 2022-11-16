9127c74c9f055c6944eb7cf7922539c82360bb64
Giorgio <gio.ivaldi@gmail.com>
2022-11-16 11:43:27 +0100

    Try to hide Layout : Done

--- MainApp/app/src/main/java/com/example/myapp/activities/MainInstructorActivity.kt
+++ MainApp/app/src/main/java/com/example/myapp/activities/MainInstructorActivity.kt
@@ -3,6 +3,8 @@ package com.example.myapp.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.LinearLayout
 import android.widget.TextView
 import com.example.myapp.R
 import com.example.myapp.models.FirebaseDbWrapper
@@ -23,12 +25,24 @@ class MainInstructorActivity : AppCompatActivity() {
         instructorFlag = intent.extras!!.getBoolean("flag")
         instructorId = intent.getStringExtra("id")
         val text1 : TextView = findViewById(R.id.textView12)
-        val text2 : TextView = findViewById(R.id.textView23)
-        text1.text = null
-        text2.text = instructorFlag.toString()
-        val firebaseDbWrapper =FirebaseDbWrapper(this)
+        text1.text = instructorFlag.toString()
+        val lay : LinearLayout = findViewById(R.id.layswitch)
+        lay.visibility= View.GONE
+        var flagvis : Boolean = false
+        text1.setOnClickListener(object : View.OnClickListener {
+            override fun onClick(v: View?) {
+                flagvis = !flagvis
+                if (flagvis){
+                    lay.visibility=View.VISIBLE
+                }
+                else {
+                    lay.visibility=View.GONE
+                }
+            }
+        })
+      /*  val firebaseDbWrapper =FirebaseDbWrapper(this)
         GlobalScope.launch(Dispatchers.IO) {
             firebaseDbWrapper.getBookings(instructorId!!, 23,11,2022)
-        }
+        }*/
     }
 }
